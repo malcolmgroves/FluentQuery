2f60d1483c6c9f8a01524417c78b6bc2e855fc16
Malcolm Groves <malcolm.r.groves@gmail.com>
2014-01-10 15:11:00 +1100

    Added License details

--- FluentQuery.Core.EnumerationStrategies.pas
+++ FluentQuery.Core.EnumerationStrategies.pas
@@ -1,3 +1,22 @@
+{****************************************************}
+{                                                    }
+{  FluentQuery                                       }
+{                                                    }
+{  Copyright (C) 2013 Malcolm Groves                 }
+{                                                    }
+{  http://www.malcolmgroves.com                      }
+{                                                    }
+{****************************************************}
+{                                                    }
+{  This Source Code Form is subject to the terms of  }
+{  the Mozilla Public License, v. 2.0. If a copy of  }
+{  the MPL was not distributed with this file, You   }
+{  can obtain one at                                 }
+{                                                    }
+{  http://mozilla.org/MPL/2.0/                       }
+{                                                    }
+{****************************************************}
+
 unit FluentQuery.Core.EnumerationStrategies;
 
 interface
