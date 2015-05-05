dcb80f16c2c0b8eb2df1d49a31a83719eb6c4502
Malcolm <malcolm.r.groves@gmail.com>
2015-05-05 15:03:55 +1000

    Simplified Tests on CharQuery significantly by using the new Count operator. Reduced lines in Test unit by around half, some test coverage.

--- tests/FluentQuery.Chars.Tests.pas
+++ tests/FluentQuery.Chars.Tests.pas
@@ -67,9 +67,6 @@ type
 
 implementation
 
-var
-  DummyChar : Char; // used to suppress wanrng about unused loop variable in tests
-
 { TestTQueryChar }
 
 procedure TestTQueryChar.NotEquals;
@@ -111,253 +108,104 @@ begin
 end;
 
 procedure TestTQueryChar.TestIsDigit;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsDigit do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 10, 'IsDigit Query should enumerate 10 chars');
+  Check(CharQuery.From(FStringVal).IsDigit.Count = 10, 'IsDigit Query should enumerate 10 chars');
 end;
 
 procedure TestTQueryChar.TestIsInArray;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsInArray(['1', '2', ')', '+', 'a']) do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 4, 'IsInArray Query should enumerate 4 chars');
+  Check(CharQuery.From(FStringVal).IsInArray(['1', '2', ')', '+', 'a']).Count = 4, 'IsInArray Query should enumerate 4 chars');
 end;
 
 procedure TestTQueryChar.TestIsLetter;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsLetter do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 51, 'IsLetter Query should enumerate 51 chars');
+  Check(CharQuery.From(FStringVal).IsLetter.Count = 51, 'IsLetter Query should enumerate 51 chars');
 end;
 
 procedure TestTQueryChar.TestIsLetterOrDigit;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsLetterOrDigit do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 61, 'IsLetterOrDigit Query should enumerate 61 chars');
+  Check(CharQuery.From(FStringVal).IsLetterOrDigit.Count = 61, 'IsLetterOrDigit Query should enumerate 61 chars');
 end;
 
 procedure TestTQueryChar.TestIsLower;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsLower do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 25, 'IsLower Query should enumerate 25 chars');
+  Check(CharQuery.From(FStringVal).IsLower.Count = 25, 'IsLower Query should enumerate 25 chars');
 end;
 
 procedure TestTQueryChar.TestIsNumber;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsNumber do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 10, 'IsNumber Query should enumerate 10 chars');
+  Check(CharQuery.From(FStringVal).IsNumber.Count = 10, 'IsNumber Query should enumerate 10 chars');
 end;
 
 procedure TestTQueryChar.TestIsPunctuation;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsPunctuation do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 8, 'IsPunctuation Query should enumerate 8 chars');
+  Check(CharQuery.From(FStringVal).IsPunctuation.Count = 8, 'IsPunctuation Query should enumerate 8 chars');
 end;
 
 procedure TestTQueryChar.TestIsSymbol;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsSymbol do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 2, 'IsSymbol Query should enumerate 2 chars');
+  Check(CharQuery.From(FStringVal).IsSymbol.Count = 2, 'IsSymbol Query should enumerate 2 chars');
 end;
 
 procedure TestTQueryChar.TestIsUpper;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsUpper do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 26, 'IsUpper Query should enumerate 26 chars');
+  Check(CharQuery.From(FStringVal).IsUpper.Count = 26, 'IsUpper Query should enumerate 26 chars');
 end;
 
 procedure TestTQueryChar.TestIsUpperSkipTake;
 var
-  LChar : Char;
   LResultString : String;
 begin
-  for LChar in CharQuery
-                 .From(FStringVal)
-                 .IsUpper
-                 .Skip(5)
-                 .Take(4) do
-  begin
-    LResultString := LResultString + LChar;
-  end;
+  LResultString := CharQuery
+                     .From(FStringVal)
+                     .IsUpper
+                     .Skip(5)
+                     .Take(4)
+                     .AsString;
   Check(LResultString.Length = 4, 'Take(4) should enumerate 4 chars');
   Check(LResultString = 'FGHI', 'IsUpper.Skip(5).Take(4) should enumerate F, G, H and then I');
 end;
 
 procedure TestTQueryChar.TestIsWhiteSpace;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).IsWhiteSpace do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 3, 'IsWhitespace Query should enumerate 3 chars');
+  Check(CharQuery.From(FStringVal).IsWhiteSpace.Count = 3, 'IsWhitespace Query should enumerate 3 chars');
 end;
 
 procedure TestTQueryChar.TestMatchesCaseInsensitive;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).Matches('Y') do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 2, 'Case Insensitive Matches Query should enumerate two chars');
+  Check(CharQuery.From(FStringVal).Matches('Y').Count = 2, 'Case Insensitive Matches Query should enumerate two chars');
 end;
 
 procedure TestTQueryChar.TestMatchesCaseSensitive;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).Matches('y', False) do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 1, 'Case Sensitive Matches Query should enumerate one char');
+  Check(CharQuery.From(FStringVal).Matches('y', False).Count = 1, 'Case Sensitive Matches Query should enumerate one char');
 end;
 
 procedure TestTQueryChar.TestNoMatchesCaseSensitive;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).Matches('z', False) do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = 0, 'Case Sensitive Matches Query with no matches should enumerate zero chars');
+  Check(CharQuery.From(FStringVal).Matches('z', False).Count = 0, 'Case Sensitive Matches Query with no matches should enumerate zero chars');
 end;
 
 procedure TestTQueryChar.TestWhereNotIsNumber;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).WhereNot(CharQuery.IsNumber) do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = FStringVal.Length - 10, 'Not(IsNumber) Query should enumerate all but 10 chars');
+  Check(CharQuery.From(FStringVal).WhereNot(CharQuery.IsNumber).Count = FStringVal.Length - 10, 'Not(IsNumber) Query should enumerate all but 10 chars');
 end;
 
 procedure TestTQueryChar.TestPassthrough;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal) do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = FStringVal.Length, 'Passthrough Query should enumerate all items');
+  Check(CharQuery.From(FStringVal).Count = FStringVal.Length, 'Passthrough Query should enumerate all items');
 end;
 
 
 procedure TestTQueryChar.TestSkipWhileQueryIsLetter;
-var
-  LPassCount : Integer;
-  LChar : Char;
 begin
-  LPassCount := 0;
-  for LChar in CharQuery.From(FStringVal).SkipWhile(CharQuery.IsLetter) do
-  begin
-    Inc(LPassCount);
-    DummyChar := LChar; // suppress warning about LChar being unused
-  end;
-  Check(LPassCount = FStringVal.Length - 25, 'Skipwhile(IsLetter) Query should enumerate all items except the first 25');
+  Check(CharQuery.From(FStringVal).SkipWhile(CharQuery.IsLetter).Count = FStringVal.Length - 25, 'Skipwhile(IsLetter) Query should enumerate all items except the first 25');
 end;
 
 function TestFluentCharPredicate.AcceptableCount(
   CharPredicate: TPredicate<Char>): Integer;
 var
   CharList : TList<Char>;
-  Value : Char;
-  PassCount : Integer;
 begin
   CharList := TList<Char>.Create;
   try
@@ -370,15 +218,7 @@ begin
     CharList.Add('1');
     CharList.Add('2');
 
-    PassCount := 0;
-
-    for Value in CharQuery.From(CharList).Where(CharPredicate) do
-    begin
-      Inc(PassCount);
-      DummyChar := Value; // suppress warning about LChar being unused
-    end;
-
-    Result := PassCount;
+    Result := CharQuery.From(CharList).Where(CharPredicate).Count;
   finally
     CharList.Free;
   end;
